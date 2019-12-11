1b9b80bdc88eeebd4349351539db093485dd4276
Edmond <emyliu@edu.uwaterloo.ca>
2019-12-11 13:37:18 -0500

    particles working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const particlesJSON = {
+window.onload = () => { const particlesJSON = {
     "particles": {
             "number": {
-                "value": 50,
+                "value": 95,
                 "density": {
                     "enable": true,
                     "value_area": 700 //Denser the smaller the number.
@@ -26,76 +26,32 @@ const particlesJSON = {
                 }
             },
             "opacity": {
-                "value": 0.7,
+                "value": 0.2,
                 "random": true
             },
             "size": {
-                "value": 10,
+                "value": 3,
                 "random": true
             },
             "line_linked": {
-                "enable": true,
+                "enable": false,
                 "distance": 200, //The radius before a line is added, the lower the number the more lines.
                 "color": "#007ecc",
-                "opacity": 0.5,
-                "width": 2
+                "opacity": 0.3,
+                "width": 0.5
             },
             "move": {
                 "enable": true,
-                "speed": 2,
+                "speed": 1.5,
                 "direction": "top", //Move them off the canvas, either "none", "top", "right", "bottom", "left", "top-right", "bottom-right" et cetera...
                 "random": true,
                 "straight": false, //Whether they'll shift left and right while moving.
                 "out_mode": "out", //What it'll do when it reaches the end of the canvas, either "out" or "bounce".
                 "bounce": false, 
-                "attract": { //Make them start to clump together while moving.
-                    "enable": true,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-      //Negate the default interactivity
-      "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": false,
-                    "mode": "repulse"
-                },
-                "onclick": {
-                    "enable": false,
-                    "mode": "push"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 800,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 800,
-                    "size": 80,
-                    "duration": 2,
-                    "opacity": 0.8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 400,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
             }
         },
         "retina_detect": true
     }
     
     particlesJS("particles-js", particlesJSON)
+}
